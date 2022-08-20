8360a693fac780d5427f95047c4eb5a78d1a27ac
eyasir454@gmail.com <you@example.com>
2022-08-20 16:51:34 +0600

    update css

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>IVMS</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link type="text/css" rel="stylesheet" href="css/style.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="colum">
+                    Welcome to IVMS
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="d-grid col-6 justify-content-md-end">
+                <a href="login.php">
+                    <button class="btn btn-danger me-md-2" type="button">Login</button>
+                </a>
+            </div>
+            <div class="d-grid col-6 justify-content-md-start">
+                <a href="register.php">
+                    <button class="btn btn-success me-md-2" type="button">Register</button>
+                </a>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
